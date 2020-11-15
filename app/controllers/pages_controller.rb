8ca75f6796bf605e38f4c9a6f98c9e4c6dac569e
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-15 23:58:12 +0100

    FEAT: Added search bar to contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
 
   def about; end
 
-  def contact; end
+  def contact
+   @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+   if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
+  end
 end
